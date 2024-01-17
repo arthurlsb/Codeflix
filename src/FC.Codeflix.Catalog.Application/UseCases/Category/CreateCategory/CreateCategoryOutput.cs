eb3e7fb637c768976ba279bbae5d0b6cf73068de
arthurlsb <lisboa.arthur159@gmail.com>
2024-01-17 11:12:46 -0300

    Refactoring use case with mediatr

--- src/FC.Codeflix.Catalog.Application/UseCases/Category/CreateCategory/CreateCategoryOutput.cs
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/CreateCategory/CreateCategoryOutput.cs
@@ -1,3 +1,5 @@
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
 namespace FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 
 public class CreateCategoryOutput
@@ -23,4 +25,13 @@ public class CreateCategoryOutput
         IsActive = isActive;
         CreatedAt = createdAt;
     }
+    
+    public static CreateCategoryOutput FromCategory(DomainEntity.Category category) 
+        => new (
+            category.Id,
+            category.Name,
+            category.Description,
+            category.IsActive,
+            category.CreatedAt
+        );
 }
