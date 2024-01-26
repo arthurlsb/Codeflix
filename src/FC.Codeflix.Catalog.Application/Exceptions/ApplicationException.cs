8f3689bcfa92af651f2fce2e19b1358bfcd197c8
arthurlsb <lisboa.arthur159@gmail.com>
2024-01-26 11:52:13 -0300

    GetCategory Exception test

---
+++ src/FC.Codeflix.Catalog.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,8 @@
+namespace FC.Codeflix.Catalog.Application.Exceptions;
+
+public abstract class ApplicationException : Exception
+{
+    protected ApplicationException(string? message) : base(message)
+    {
+    }
+}
