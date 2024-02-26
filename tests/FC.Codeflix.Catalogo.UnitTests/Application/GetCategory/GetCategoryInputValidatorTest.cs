da9ceacaf412eeecbefe664818358c3911725d95
arthurlsb <lisboa.arthur159@gmail.com>
2024-02-26 09:18:08 -0300

    Finalizing GetCategory Validation

--- tests/FC.Codeflix.Catalogo.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
+++ tests/FC.Codeflix.Catalogo.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -27,4 +27,20 @@ public class GetCategoryInputValidatorTest
         validationResult.IsValid.Should().BeTrue();
         validationResult.Errors.Should().HaveCount(0);
     }
+    
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void InvalidWhenEmptyGuidId()
+    {
+        var validInput = new GetCategoryInput(Guid.Empty);
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+        
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeFalse();
+        validationResult.Errors.Should().HaveCount(1);
+        validationResult.Errors[0].ErrorMessage.Should().Be("'Id' deve ser informado.");
+
+    }
 }
