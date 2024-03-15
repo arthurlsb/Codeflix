8003a01c138dc1207844088376a3fcce464e1fe4
arthurlsb <lisboa.arthur159@gmail.com>
2024-03-15 15:33:19 -0300

    List categories test assert

--- tests/FC.Codeflix.Catalogo.UnitTests/Application/ListCategories/ListCategoriesTest.cs
+++ tests/FC.Codeflix.Catalogo.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using FC.Codeflix.Catalog.Domain.Entity;
 using Moq;
 using Xunit;
@@ -28,6 +27,12 @@ public class ListCategoriesTest
             sort: "name",
             dir: SearchOrder.Asc
         );
+        var outputRepositorySearch = new OutputSearch<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            Items: (IReadOnlyList<Category>)categoriesExampleList,
+            Total: 70
+        );
         repositoryMock.Setup(x => x.Search(
             It.Is<SearchInput>(
                 searchInput.Page == input.Page
@@ -37,14 +42,35 @@ public class ListCategoriesTest
                 && searchInput.Order == input.Dir
             ),
             It.IsAny<CancellationToken>()
-        )).ReturnsAsync(new OutputSearch<Category>(
-            currentPage: input.Page, 
-            perPage: input.PerPage,
-            Items: (IReadOnlyList<Category>) categoriesExampleList,
-            Total: 70
-        ));
+        )).ReturnsAsync(outputRepositorySearch);
         var useCase = new ListCategories(repositoryMock.Object);
 
         var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Page.Should().Be(outputRepositorySearch.currentPage);
+        output.PerPage.Should().Be(outputRepositorySearch.PerPage);
+        output.Items.Should().HaveCount(outputRepositorySearch.Items.Count);
+        output.Total.Should().Be(outputRepositorySearch.Total);
+        output.Items.Foreach(outputItem =>
+        {
+            var repositoryCategory = outputRepositorySearch
+                .Items.Find(x => x.Id == outputItem.Id);
+            outputItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(repositoryCategory.Name);
+            outputItem.Description.Should().Be(repositoryCategory.Description);
+            outputItem.IsActive.Should().Be(repositoryCategory.IsActive);
+            outputItem.CreatedAt.Should().Be(repositoryCategory.CreatedAt);
+        });
+        repositoryMock.Verify(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
     }
 }
